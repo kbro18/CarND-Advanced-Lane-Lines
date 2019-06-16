5359f1e84e51263daed7821a326832ac1de2a24e
kbro18 <karl.bromfield@me.com>
2019-06-16 22:21:15 +1000

    working with curvature, todo: add look ahead etc and lane obj implemetnation

--- utils.py
+++ utils.py
@@ -2,6 +2,79 @@ import cv2
 import numpy as np
 import matplotlib.image as mpimg
 
+def threshold_process(img, img_name, ksize=7, s_thresh=(180, 255), sx_thresh=(25, 255), sy_thresh=(15, 255), mag_thresh=(80, 255), dir_thresh=(0.8, 1.3)):
+    img = np.copy(img)
+    hls_img = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+    
+    # individual color channels:
+    gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    l_channel = hls_img[:,:,1]
+    s_channel = hls_img[:,:,2]
+    
+    gradx = abs_sobel_thresh(l_channel,'x', ksize, sx_thresh)
+    grady = abs_sobel_thresh(l_channel,'y', ksize, sy_thresh)
+    mag_binary = mag_threshold(gray_img, ksize, mag_thresh)
+    dir_binary = dir_threshold(gray_img, ksize, dir_thresh)
+    
+    c_chan_binary = color_threshold(s_channel, s_thresh)
+    
+    # combine sobel masks
+    gradxy = combine_masks(gradx, grady, "and")
+    
+    # combine sobel and color masks
+    combined = combine_masks(gradxy, c_chan_binary, "or")
+    
+    return combined
+
+def abs_sobel_thresh(img, orient='x', sobel_kernel=3, sobel_thresh=(0, 255)):
+    # Calculate directional gradient
+    if orient == "x":
+        abs_sobel = np.absolute(cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel)) # Take the derivative in x
+    else:
+        abs_sobel = np.absolute(cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel)) # Take the derivative in x
+    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+    # Apply threshold
+    gradient_binary = np.zeros_like(scaled_sobel)
+    gradient_binary[(scaled_sobel >= sobel_thresh[0]) & (scaled_sobel <= sobel_thresh[1])] = 1
+    return gradient_binary
+
+def mag_threshold(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    # Calculate gradient magnitude
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    gradmag = np.sqrt(sobelx**2 + sobely**2)
+    scale_factor = np.max(gradmag)/255 
+    gradmag = (gradmag/scale_factor).astype(np.uint8) 
+    # Apply threshold
+    mag_binary = np.zeros_like(gradmag)
+    mag_binary[(gradmag >= mag_thresh[0]) & (gradmag <= mag_thresh[1])] = 1
+    return mag_binary
+
+def dir_threshold(img, sobel_kernel=3, dir_thresh=(0, np.pi/2)):
+    # Calculate gradient direction
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    absgraddir = np.arctan2(np.absolute(sobely), np.absolute(sobelx))
+    # Apply threshold
+    dir_binary =  np.zeros_like(absgraddir)
+    dir_binary[(absgraddir >= dir_thresh[0]) & (absgraddir <= dir_thresh[1])] = 1
+    return dir_binary
+
+def color_threshold(col_channel, color_thresh=(0,255)):
+    col_binary = np.zeros_like(col_channel)
+    col_binary[(col_channel >= color_thresh[0]) & (col_channel <= color_thresh[1])] = 1
+    return col_binary
+
+def combine_masks(m1, m2, logic):
+    combined = np.zeros_like(m1)
+    if logic == "and":
+        combined[(m1 == 1) & (m2 == 1)] = 1
+        return combined
+    if logic == "or":
+        combined[(m1 == 1) | (m2 == 1)] = 1
+        return combined
+    return None
+
 def weighted_img(img, initial_img, α=0.8, β=1., γ=0.):
     """
     `img` is the output of the hough_lines(), An image with lines drawn on it.
@@ -71,6 +144,7 @@ def warp(img, img_name):
     #cv2.imwrite("./test_images_output/" + img_name[:-4] +"/03_" + img_name[:-4] + "_masked.jpg" , cv2.cvtColor(masked_img, cv2.COLOR_BGR2RGB))
 
     M = cv2.getPerspectiveTransform(src, dst)
+    Minv = cv2.getPerspectiveTransform(dst, src)
     warped_img = cv2.warpPerspective(img, M, (imshape[1],imshape[0]))
     cv2.imwrite("./test_images_output/" + img_name[:-4] +"/04_" + img_name[:-4] + "_warped.jpg" , cv2.cvtColor(warped_img, cv2.COLOR_BGR2RGB))
-    return warped_img
+    return warped_img, M, Minv
